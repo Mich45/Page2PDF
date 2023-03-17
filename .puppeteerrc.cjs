bff65dafd8435e5c3b21e5dfcf18bbef5cb30585
Michael Hungbo <hungbomichael@gmail.com>
2023-03-17 19:52:42 +0100

    fix: fix spinner

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: 'C:\\Users\\A\\.cache\\puppeteer',
+  cacheDirectory: 'C:\Users\A\.cache\puppeteer',
 };
